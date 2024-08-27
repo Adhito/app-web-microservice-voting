330c098109142cf309043aaaee28b0db87d79c93
Adhito <adhito909@gmail.com>
2024-08-27 21:10:37 +0700

    Update script health check  to not use local unix socket

--- scripts-healthchecks/script_healthcheck_postgres.sh
+++ scripts-healthchecks/script_healthcheck_postgres.sh
@@ -7,3 +7,11 @@ host="$(hostname -i || echo '127.0.0.1')"
 user="${POSTGRES_USER:-postgres}"
 db="${POSTGRES_DB:-$POSTGRES_USER}"
 export PGPASSWORD="${POSTGRES_PASSWORD:-}"
+
+## Create a set of arguments for a command for postgres to not use the local unix socket 
+args=(
+	--host "$host"
+	--username "$user"
+	--dbname "$db"
+	--quiet --no-align --tuples-only
+)
